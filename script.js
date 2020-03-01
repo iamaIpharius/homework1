5aae3f7d445c2dc27bcad4fcc1b9df4748c43798
Alex <sturmmanngr@gmail.com>
2020-03-01 21:49:56 +0700

    second homework

--- script.js
+++ script.js
@@ -13,16 +13,54 @@ let appData =  {
     income : [],
     savings: false,
 };
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
 
+// for (let i = 0; i < 2; i++) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//     b = prompt("Во сколько обойдется?", '');
+    
+//     if (typeof(a)=== 'string' && typeof(a) != null && a != '' && b != '' && a.length < 50) {
+//         console.log("done");
+//         appData.expenses[a] = b;
+//     } else {
+//        i = 0;
+//        console.log(i);
+//     }
+// };
+// let i = 0;
+// while (i < 2) {
+//     i++;
+    
+//         let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//             b = prompt("Во сколько обойдется?", '');
+//             if (typeof(a)=== 'string' && typeof(a) != null && a != '' && b != '' && a.length < 10) {
+//                 console.log("done");
+//                 appData.expenses[a] = b;
+//         } else {
+//             i = 0;
+//             console.log(i);
+//         };
+    
+// };
+let i = 0;
+do {
+    
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+        i++;
+        console.log('i=' + i);
+    if (typeof(a)=== 'string' && typeof(a) != null && a != '' && b != '' && a.length < 10) {
+            console.log("done");
+            appData.expenses[a] = b;
+    } else {
+            i = 0;
+            console.log(i);
+    };
+     
+
+}while(i < 2);
 
 console.log(appData);
-const oneDayBudget = money / 30;
+const oneDayBudget = appData.budget / 30;
 console.log(oneDayBudget);
-alert("Бюджет на 1 день" + oneDayBudget);
+alert("Бюджет на 1 день " + oneDayBudget);
